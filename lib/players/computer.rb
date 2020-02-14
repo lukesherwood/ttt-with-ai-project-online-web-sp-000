f5e59d147c9e61e653963e8d9296ce850951563a
Luke Sherwood <luke.sherwood55@gmail.com>
2020-02-14 21:51:15 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,55 +1,46 @@
 require 'pry'
+#ruby bin/tictactoe.rb
 module Players
-  
 class Players::Computer < Player
-#ruby bin/tictactoe.rb  
-WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8], # Bottom row
-  [0,3,6], #left column
-  [1,4,7], # middle column
-  [2,5,8], #right column
-  [0,4,8], #diagonal 1
-  [2,4,6] #diagonal 2
+  attr_accessor :board
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
   ]
-  def move(board)
-   #center position
-  
-    if !board.taken?("5")
-    "5"
-    else
-      
-    computer_move(board)
+  CORNERS = [0, 2, 8, 6]
+
+  def move(board, timer = 0)
+    @board = board
+    timer <= 0 ? @timer = 0.01 : @timer = timer
+    case
+      when winning_move != nil
+        input = winning_move + 1
+      when blocking_move != nil
+        input = blocking_move + 1
+      when center?
+        input = 5
+      when opposite_corner != nil && board.taken?(opposite_corner) == false
+        input = opposite_corner
+      when corner
+        input = corner + 1
+      else
+        until !board.taken?(input)
+          input = (1..9).to_a.sample
+        end
     end
+    sleep(@timer)
+    input.to_s
   end
-
-  def computer_move(board)
-    win_move(board) || block(board) || corner(board) || middle_corner(board)
+  def other
+    token == "X" ? "O" : "X"
   end
-
-  def middle_corner(board)
-    middle_corners = [2, 4, 6, 8]
-    middle_corners.find{|n| !board.taken?(n)}
-  end
-
-  def corner(board)
-    corners =  [1, 3, 7, 9]
-    corners.find{|n| !board.taken?(n)}
-  end
-
-  def block(board)
-   
-  end
-  
-   def other_symbol
-    case symbol
-    when 'X' then 'O'
-    else 'X'
-    end
-  end
-
- def win_move(board)
+  def winning_move
     winning_row = WIN_COMBINATIONS.find do |combo|
       (board.cells[combo[0]] == token && board.cells[combo[1]] == token && board.cells[combo[2]] == " ") || (board.cells[combo[2]] == token && board.cells[combo[1]] == token && board.cells[combo[0]] == " ") || (board.cells[combo[0]] == token && board.cells[combo[2]] == token && board.cells[combo[1]] == " ")
     end
@@ -57,6 +48,36 @@ WIN_COMBINATIONS = [
       winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
     end
   end
-  
+  def blocking_move
+    winning_row = WIN_COMBINATIONS.find do |combo|
+      (board.cells[combo[0]] == other && board.cells[combo[1]] == other && board.cells[combo[2]] == " ") || (board.cells[combo[1]] == other && board.cells[combo[2]] == other && board.cells[combo[0]] == " ") || (board.cells[combo[2]] == other && board.cells[combo[0]] == other && board.cells[combo[1]] == " ")
+    end
+    if winning_row != nil
+      winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
+    end
+  end
+  def center?
+    board.cells[4] == " "
+  end
+  def corners
+    CORNERS.shuffle!
+  end
+  def corner
+    corners.find {|corner| board.cells[corner] == " "}
+  end
+  def opposite_corner
+    case
+      when board.taken?(1) && !board.taken?(9)
+        9
+      when board.taken?(9) && !board.taken?(1)
+        1
+      when board.taken?(3) && !board.taken?(7)
+        7
+      when board.taken?(7) && !board.taken?(3)
+        3
+      else
+        nil
+    end
+  end
 end
 end
